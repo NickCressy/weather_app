29ee972eb40530e79cb9c7d76a2bafc7a8be3bed
nick <nscressy@csuchico.edu>
2025-04-02 10:48:11 -0700

    Sort of working. If you don't have an account you can create an account and login. It stays logged in forever. Can add cities to your account on the homepage. Weather api not implemented yet.

--- weather/views.py
+++ weather/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .models import Location
 from .forms import LocationForm
+import requests
 
 def signup(request):
     if request.method == 'POST':
@@ -15,16 +16,30 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'weather/signup.html', {'form': form})
 
+def get_weather(city):
+    api_key = 'key'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    response = requests.get(url)
+    return response.json() if response.status_code == 200 else None
+
 def home(request):
     if request.user.is_authenticated:
         locations = Location.objects.filter(user=request.user)
-        if form.is_valid():
-            if locations.count() < 10:
-                form.instance.user = request.user
-                form.save()
-                return redirect('home')
+        weather_data = []
+        for location in locations:
+            weather = get_weather(location.city)
+            if weather:
+                weather_data.append({'city': location.city, 'weather': weather})
+        
+        if request.method == 'POST':
+            form = LocationForm(request.POST)
+            if form.is_valid():
+                if locations.count() < 10:
+                    form.instance.user = request.user
+                    form.save()
+                    return redirect('home')
         else:
             form = LocationForm()
-        return render(request, 'weather/home.html', {'locations': locations, 'form': form})
+        return render(request, 'weather/home.html', {'locations': locations, 'form': form, 'weather_data': weather_data})
     else:
         return redirect('login')
--- weather_app/urls.py
+++ weather_app/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from weather import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('weather/', include('weather.urls')),
+    path('', views.home, name='home'),
 ]
